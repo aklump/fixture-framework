4cb412f2af863301218c848da292abb6e1111c80
Aaron Klump <sourcecode@intheloftstudios.com>
2026-04-28 08:39:10 -0700

    Fix some documentation errors

--- knowledge/snippet/run_context.php
+++ knowledge/snippet/run_context.php
@@ -4,8 +4,8 @@
 class FixtureA extends \AKlump\FixtureFramework\AbstractFixture {
 
   public function __invoke(): void {
-    $this->runContext->set('fixture_a.required_data', 'lorem');
-    $this->runContext->set('fixture_a.optional_data', 'ipsum');
+    $this->context()->set('fixture_a.required_data', 'lorem');
+    $this->context()->set('fixture_a.optional_data', 'ipsum');
   }
 }
 
@@ -14,10 +14,10 @@ class FixtureB extends \AKlump\FixtureFramework\AbstractFixture {
 
   public function __invoke(): void {
     // This will throw an exception if the required data is not set.
-    $lorem = $this->runContext->require('fixture_a.required_data');
+    $lorem = $this->context()->require('fixture_a.required_data');
 
-    if ($this->runContext->has('fixture_a.optional_data')) {
-      $ipsum = $this->runContext->get('fixture_a.optional_data');
+    if ($this->context()->has('fixture_a.optional_data')) {
+      $ipsum = $this->context()->get('fixture_a.optional_data');
       // Do something with $ipsum, which may be an empty string. the `has()`
       // method only checks for existence, not value.
     }
